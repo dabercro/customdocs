6b86673291ed24bfe4b383f6db7d5e5eb0c5ef59
dabercro <dabercro@mit.edu>
2018-08-04 11:02:01 -0400

    Added a function to make executable scripts with OptionParsers pretty in Sphinx

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 import setuptools
 
+from customdocs import __version__
+
 setuptools.setup(
     name='customdocs',
-    version='0.1.0',
+    version=__version__,
     packages=setuptools.find_packages(),
     author='Daniel Abercrombie',
     author_email='dabercro@mit.edu',
     description='Custom parsers for sphinx',
     url='https://github.com/dabercro/customdocs',
-    install_requires=['sphinxcontrib-autoanysrc']
+    install_requires=[
+        'sphinx',
+        'sphinxcontrib-autoanysrc']
     )
